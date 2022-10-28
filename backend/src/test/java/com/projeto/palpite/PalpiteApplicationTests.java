7679ac0b1f61ae0ddfc5fa4e368a9028b63de0fa
Isaac Nogueira <isaac.nogueira@icloud.com>
2022-10-28 17:40:10 -0300

    Criação projeto

---
+++ backend/src/test/java/com/projeto/palpite/PalpiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.palpite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalpiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
